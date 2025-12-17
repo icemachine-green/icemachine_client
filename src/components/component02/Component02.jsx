4f21b44b1fc852ba6cb14c37219bd566aabe0c0a
taeholee950 <inwoo950@gmail.com>
2025-12-17 14:56:50 +0900

    251217 MAIN01_COMPONENT02 완료

---
+++ src/components/component02/Component02.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Component02.css";
+
+const Component02 = () => {
+  return (
+    <div className="component02-container">
+      <div className="component02-text">
+        <h1 className="component02-title">제빙기 케어, 왜 필요할까요?</h1>
+        <div className="component02-separator-bar"></div>
+        <p className="component02-paragraph">
+          "제빙기 청소는 얼음의 위생과 맛을 지키는 기본입니다.
+          <br />
+          정기적인 관리로 세균 오염을 막고 고객에게 신뢰를 제공합니다."
+        </p>
+      </div>
+      <img
+        src="/icons/icecare.png"
+        alt="Ice Care Service"
+        className="component02-image"
+      />
+    </div>
+  );
+};
+
+export default Component02;
--- src/components/main/MainPage.jsx
+++ src/components/main/MainPage.jsx
@@ -1,12 +1,17 @@
-import React from "react";
-import "./MainPage.css";
+import React from 'react';
+import './MainPage.css';
+import Component02 from '../component02/Component02';
 
 const MainPage = () => {
   return (
-    <div className="mainpage-container">
-      <h1>메인 페이지</h1>
-    </div>
+    <>
+      <div className="mainpage-container">
+        <h1>메인 페이지</h1>
+      </div>
+      <Component02 />
+    </>
   );
 };
 
 export default MainPage;
+
