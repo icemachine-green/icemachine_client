ec87820030c19c5433b8ad5fc7cefe9d66329f4b
kimjunghyun <john0210@hanmail.net>
2025-12-17 20:49:51 +0900

    251217 MAIN01_COMPONENT04 스와이퍼 슬라이드와 배경 사이 얼음 이미지 삽입 수정 작업 완료

--- src/components/component04/Component04.jsx
+++ src/components/component04/Component04.jsx
@@ -11,6 +11,7 @@ import "./Component04.css";
 // import required modules
 import { Pagination, Navigation } from 'swiper/modules';
 
+
 const Component04 = () => {
   return (
 		<div className="component04-background">
@@ -28,14 +29,12 @@ const Component04 = () => {
         modules={[Pagination, Navigation]}
         className="mySwiper"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>작업과정1. 스팀 청소</SwiperSlide>
+        <SwiperSlide>작업과정2. 세부 청소</SwiperSlide>
+        <SwiperSlide>작업과정3. 필터 청소</SwiperSlide>
       </Swiper>
     </>
-			</div>
+		  </div>		
 		</div>
   );
 };
