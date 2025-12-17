30055f71e95a126a911de9fb4a7d8f929da2ece0
grinjk0418 <grinjk0418@gmail.com>
2025-12-17 12:53:13 +0900

    251217 헤더 컴포넌트 이미지 깨지는거 수정, (componentName-elementName 형식으로 컴포넌트 이름을 따라오도록 작성) 컨벤션 적용

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -2,22 +2,22 @@ import './Header.css';
 
 export default function Header() {
   return (
-    <header className="ice-header">
+    <header className="header-ice-header">
       {/* 얼음 로고 = 홈 버튼(지금은 동작 없이 버튼만) */}
-      <button type="button" className="ice-header__logoBtn" aria-label="홈">
+      <button type="button" className="header-ice-header__logoBtn" aria-label="홈">
         <img
-          className="ice-header__logoImg"
-          src="/icons/ssacssac-logo.png"
+          className="header-ice-header__logoImg"
+          src="/icons/mobile_homeicon.png"
           alt="홈"
         />
       </button>
 
-      <div className="ice-header__spacer" />
+      <div className="header-ice-header__spacer" />
 
       {/* 로그인 버튼(지금은 동작 없이 버튼만) */}
-      <button type="button" className="ice-header__loginBtn">
-        <img className="ice-header__loginIcon" src="/icons/login-icon.png" alt="" />
-        <span className="ice-header__loginText">LOGIN</span>
+      <button type="button" className="header-ice-header__loginBtn">
+        <img className="header-ice-header__loginIcon" src="/icons/login.png" alt="" />
+        <span className="header-ice-header__loginText">LOGIN</span>
       </button>
     </header>
   );
