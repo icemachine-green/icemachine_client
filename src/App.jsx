ba9d3f48914183ee9f76093e1d06355bde0f9f3e
taeholee950 <inwoo950@gmail.com>
2025-12-18 16:57:20 +0900

    251218 FOOTER 완료, Header 컴포넌트가 컨텐츠 영역을 가리는걸 방지하기위해 padding-top 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
 import Header from "./components/common/Header.jsx";
+import Footer from "./components/common/Footer.jsx";
 import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Header />
-      <Outlet />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
     </>
   );
 }
---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer className="footer-container">
+      <div className="footer-info">
+        <ul>
+          <li>대표자: 김정배</li>
+          <li>사업자등록번호: 504-85-25999</li>
+          <li>주소: 대구광역시 중구 중앙대로 394, 호태빌딩 2F</li>
+        </ul>
+      </div>
+      <div className="footer-logo">
+        <img src="/icons/homeicon.jpg" alt="Company Logo" />
+      </div>
+    </footer>
+  );
+}
