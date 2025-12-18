3117dcbd1a644c7de03dd65f3c8f248d89018749
kimjunghyun <john0210@hanmail.net>
2025-12-18 17:00:13 +0900

    251218 LOGIN_PAGE 완료

--- src/components/login/LoginPage.jsx
+++ src/components/login/LoginPage.jsx
@@ -1,17 +1,23 @@
 import React from "react";
+import './LoginPage.css';
 
 const LoginPage = () => {
   return (
-    <div>
-      <h1>로그인페이지</h1>
-      <h1>로그인페이지</h1>
-      <h1>로그인페이지</h1>
-      <h1>로그인페이지</h1>
-      <h1>로그인페이지</h1>
-      <h1>로그인페이지</h1>
-      <h1>로그인페이지</h1>
+    <div className="login-page">
+      <div className="login-container">
+        {/* 로고 영역 */}
+        <div className="login-logo">
+          <img src="/icons/logoonly.png" alt="싹싹이 로고" />
+        </div>
+
+        {/* 카카오 로그인 영역 */}
+        <div className="login-form">        
+          <img className="login-kakao" src="/icons/kakao_login.png" alt="카카오톡 로그인 이미지" />
+        </div>
+       </div>
     </div>
   );
 };
+
 
 export default LoginPage;
