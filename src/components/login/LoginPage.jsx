6c8e24cfa4f882ed9830494f3308f22778b9173c
taeholee950 <inwoo950@gmail.com>
2025-12-26 10:48:41 +0900

    251226 SignUp 컴포넌트 추가. LoginPage 컴포넌트의 '카카오로 시작하기' 클릭 시 SignUp 컴포넌트로 redirect 처리 라우터 등록 완료.

--- src/components/login/LoginPage.jsx
+++ src/components/login/LoginPage.jsx
@@ -1,7 +1,14 @@
 import React from "react";
-import './LoginPage.css';
+import { useNavigate } from "react-router-dom";
+import "./LoginPage.css";
 
 const LoginPage = () => {
+  const navigate = useNavigate();
+
+  // /signup으로 리다이렉트
+  const handleRedirectToSignup = () => {
+    navigate("/signup");
+  };
   return (
     <div className="login-page">
       <div className="login-container">
@@ -11,13 +18,17 @@ const LoginPage = () => {
         </div>
 
         {/* 카카오 로그인 영역 */}
-        <div className="login-form">        
-          <img className="login-kakao" src="/icons/kakao_login.png" alt="카카오톡 로그인 이미지" />
+        <div className="login-form">
+          <img
+            className="login-kakao"
+            src="/icons/kakao_login.png"
+            alt="카카오톡 로그인 이미지"
+            onClick={handleRedirectToSignup}
+          />
         </div>
-       </div>
+      </div>
     </div>
   );
 };
-
 
 export default LoginPage;
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,24 +1,29 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import App from '../App.jsx';
-import MainPage from '../components/main/MainPage.jsx';
-import ReviewPage from '../components/reviews/ReviewPage.jsx';
-import LoginPage from '../components/login/LoginPage.jsx';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "../App.jsx";
+import MainPage from "../components/main/MainPage.jsx";
+import ReviewPage from "../components/reviews/ReviewPage.jsx";
+import LoginPage from "../components/login/LoginPage.jsx";
+import SignUp from "../components/signup/SignUp.jsx";
 
 const router = createBrowserRouter([
   {
     element: <App />,
     children: [
       {
-        path: '/',
+        path: "/",
         element: <MainPage />,
       },
       {
-        path: '/reviews',
+        path: "/reviews",
         element: <ReviewPage />,
       },
       {
-        path: '/login',
+        path: "/login",
         element: <LoginPage />,
+      },
+      {
+        path: "/signup",
+        element: <SignUp />,
       },
       // 앞으로 다른 페이지가 추가되면 이 배열에 추가합니다.
       // 예: { path: '/login', element: <LoginPage /> }
